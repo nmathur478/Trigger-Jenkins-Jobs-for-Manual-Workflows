11d2c679c6bea3c7f4cb3458c8c9047c9322ec20
Nilesh Mathur <110603429+nmathur478@users.noreply.github.com>
2023-05-22 17:25:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import os
-
+'''
 print(f"""TEST1: {os.getenv("TEST1")}""")
 print(f"""TEST1: {os.environ.get("TEST1")}""")
 print(f"""TEST: {os.environ.get("TEST")}""")
+'''
+os.system("echo ${{vars.TEST1}}")
