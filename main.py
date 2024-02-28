8538c652c955a7224a513b7e802f409794df2c53
Nilesh Mathur <110603429+nmathur478@users.noreply.github.com>
2024-02-28 17:08:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
+# import os
+# if __name__ == "__main__":
+#     # Retrieve Jenkins secrets from environment variables
+#     jenkins_url = os.environ.get("JENKINS_URL")
+#     print(jenkins_url)
+#     # print(f"""TEST1: {os.environ.get("TEST1")}""")
+#     # print(f"""TEST: {os.environ.get("TEST")}""")
+
 import os
-if __name__ == "__main__":
-    # Retrieve Jenkins secrets from environment variables
-    jenkins_url = os.environ.get("JENKINS_URL")
-    print(jenkins_url)
-    # print(f"""TEST1: {os.environ.get("TEST1")}""")
-    # print(f"""TEST: {os.environ.get("TEST")}""")
+hostname = "jenkins.calance.work" #example
+response = os.system("ping -c 1 " + hostname)
+
+#and then check the response...
+if response == 0:
+  print(f"{hostname} is up!")
+else:
+  print(f"{hostname} is down!")
