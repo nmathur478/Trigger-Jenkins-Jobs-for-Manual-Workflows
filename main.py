d7e0a5e182cdf3871b5cd1b0a040f5522d2ee531
Nilesh Mathur <110603429+nmathur478@users.noreply.github.com>
2024-02-28 16:56:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import os
 if __name__ == "__main__":
     # Retrieve Jenkins secrets from environment variables
-    jenkins_url = os.getenv("JENKINS_URL")
-    print(jenkins_url)
+    # jenkins_url = os.getenv("JENKINS_URL")
+    # print(jenkins_url)
+    print(f"""TEST1: {os.environ.get("TEST1")}""")
+    print(f"""TEST: {os.environ.get("TEST")}""")
